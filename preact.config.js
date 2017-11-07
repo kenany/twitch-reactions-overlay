2ccbe0e1601d47119f868f11d293afb976200136
Kenan Yildirim <kenan@kenany.me>
2017-11-07 08:54:45 -0500

    build: remove babel-preset-react

--- preact.config.js
+++ preact.config.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const flowStripTypes = require('babel-plugin-transform-flow-strip-types');
 const emotionPlugin = require('babel-plugin-emotion');
 
@@ -7,14 +9,6 @@ module.exports = (config) => {
     return loader.loader === 'babel-loader';
   })[0];
 
-  babelLoader.options.presets = [
-    ['env', {
-      targets: {
-        browsers: ['last 2 versions']
-      }
-    }],
-    'react'
-  ];
   babelLoader.options.plugins.push(flowStripTypes);
   babelLoader.options.plugins.push(emotionPlugin);
 };
