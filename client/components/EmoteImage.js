6751bb25f286fcd6b22edaf3760073d8da27b98c
Kenan Yildirim <kenan@kenany.me>
2019-05-16 19:44:20 -0400

    deps: emotion@10.0.9

--- client/components/EmoteImage.js
+++ client/components/EmoteImage.js
@@ -1,11 +1,22 @@
 // @flow
+// @jsx h
 
-import styled from 'preact-emotion';
+import { h } from 'preact';
+import { css } from 'emotion';
 
-const EmoteImage = styled('img')`
-  position: absolute;
-  left: ${({ left }) => left};
-  top: ${({ top }) => top};
-`;
+type Props = {
+  left: string,
+  src: string,
+  top: string
+};
+
+const EmoteImage = ({ left, src, top }: Props) => {
+  const style = css`
+    position: absolute;
+    left: ${left};
+    top: ${top};
+  `;
+  return <img className={style} src={src} />;
+};
 
 export default EmoteImage;
