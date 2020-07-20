2bae779b5f414ef4ca07dc6ee4a6c404c6586bdf
Kenan Yildirim <kenan@kenany.me>
2020-07-19 23:13:49 -0400

    chore(build): replace default minimizer

--- preact.config.js
+++ preact.config.js
@@ -4,7 +4,9 @@ require('dotenv').config();
 
 import flowStripTypes from '@babel/plugin-transform-flow-strip-types';
 import emotionPlugin from 'babel-plugin-emotion';
+import OptimizeCssAssetsPlugin from 'optimize-css-assets-webpack-plugin';
 import process from 'process';
+import TerserPlugin from 'terser-webpack-plugin';
 
 export default (config, env, helpers) => {
   const { rule } = helpers.getLoadersByName(config, 'babel-loader')[0];
@@ -17,4 +19,33 @@ export default (config, env, helpers) => {
       'process.env.WS_ADDRESS': `"${process.env.WS_ADDRESS}"`
     })
   );
+
+  config.optimization.minimizer = [
+    new TerserPlugin({
+      cache: true,
+      extractComments: false,
+      parallel: true,
+      sourceMap: true,
+      terserOptions: {
+        compress: {
+          hoist_funs: true,
+          keep_fargs: false,
+          pure_getters: true,
+          pure_funcs: [
+            'classCallCheck',
+            '_classCallCheck',
+            '_possibleConstructorReturn',
+            'Object.freeze',
+            'invariant',
+            'warning'
+          ]
+        },
+        mangle: true,
+        output: { comments: false }
+      }
+    }),
+    new OptimizeCssAssetsPlugin({
+      cssProcessorOptions: { reduceIdents: false }
+    })
+  ];
 };
