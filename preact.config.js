313fdceee32eb6928685c81a480ca835cfd60da7
Kenan Yildirim <kenan@kenany.me>
2021-02-19 11:25:05 -0500

    chore(deps): remove custom minimizer

--- preact.config.js
+++ preact.config.js
@@ -4,9 +4,7 @@ require('dotenv').config();
 
 import flowStripTypes from '@babel/plugin-transform-flow-strip-types';
 import emotionPlugin from 'babel-plugin-emotion';
-import OptimizeCssAssetsPlugin from 'optimize-css-assets-webpack-plugin';
 import process from 'process';
-import TerserPlugin from 'terser-webpack-plugin';
 
 export default (config, env, helpers) => {
   const { rule } = helpers.getLoadersByName(config, 'babel-loader')[0];
@@ -19,33 +17,4 @@ export default (config, env, helpers) => {
       'process.env.WS_ADDRESS': `"${process.env.WS_ADDRESS}"`
     })
   );
-
-  config.optimization.minimizer = [
-    new TerserPlugin({
-      cache: true,
-      extractComments: false,
-      parallel: true,
-      sourceMap: true,
-      terserOptions: {
-        compress: {
-          hoist_funs: true,
-          keep_fargs: false,
-          pure_getters: true,
-          pure_funcs: [
-            'classCallCheck',
-            '_classCallCheck',
-            '_possibleConstructorReturn',
-            'Object.freeze',
-            'invariant',
-            'warning'
-          ]
-        },
-        mangle: true,
-        output: { comments: false }
-      }
-    }),
-    new OptimizeCssAssetsPlugin({
-      cssProcessorOptions: { reduceIdents: false }
-    })
-  ];
 };
